f60eb1ec9acf4e891fb39b9660735e68310183ef
caoaoyu <312318138@qq.com>
2017-10-19 11:14:24 +0800

    Feat: 游戏背景滚动与屏幕适配 1· 背景    双图拼接    滚动    定时 2· 屏幕适配    居中    自动竖屏    全屏显示

--- src/BackGround.js
+++ src/BackGround.js
@@ -3,11 +3,12 @@ var BackGround = (function (_super) {
         BackGround.super(this);
         this.bg1 = new Laya.Sprite();
         this.bg1.loadImage('comp/background.png');
+         this.addChild(this.bg1)
 
         this.bg2 = new Laya.Sprite();
         this.bg2.loadImage('comp/background.png');
-
-        this.addChild(this.bg1, this.bg2)
+        this.bg2.pos(0, -852)
+        this.addChild(this.bg2)
 
         Laya.timer.frameLoop(1, this, this.onAnimate);
     
@@ -17,7 +18,13 @@ var BackGround = (function (_super) {
     var _proto = BackGround.prototype;
 
     _proto.onAnimate = function () {
-        this.pivotY += 1;
+        this.y += 1;
+        if( this.bg1.y + this.y >= 852) {
+            this.bg1.y -= 852 * 2
+        }
+        if( this.bg2.y + this.y >= 852) {
+            this.bg2.y -= 852 * 2
+        }
     }
 
     return BackGround;
